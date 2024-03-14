bb307b777ef5ed43134c8758c0be6607a68ede62
WeiKai Huang <asd0021128@gmail.com>
2024-03-15 00:32:45 +0800

    create authentications.

--- src/main/java/dev/demo/demo/config/WebConfig.java
+++ src/main/java/dev/demo/demo/config/WebConfig.java
@@ -1,2 +1,20 @@
-package dev.demo.demo.config;public class WebConfig {
-}
+package dev.demo.demo.config;
+
+import dev.demo.demo.filter.AuthenticationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+//@Component
+//public class WebConfig {
+//    @Bean
+//    public FilterRegistrationBean<AuthenticationFilter>  logingFilter() {
+//        FilterRegistrationBean<AuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+//
+//        registrationBean.setFilter(new AuthenticationFilter());
+//        registrationBean.addUrlPatterns("/movie/api/*");
+//        registrationBean.setOrder(2);
+//
+//        return registrationBean;
+//    }
+//}
--- src/main/java/dev/demo/demo/filter/AuthenticationFilter.java
+++ src/main/java/dev/demo/demo/filter/AuthenticationFilter.java
@@ -1,2 +1,37 @@
-package dev.demo.demo.filter;public class AuthenticationFilter {
+package dev.demo.demo.filter;
+
+import com.google.common.hash.Hashing;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class AuthenticationFilter implements Filter {
+    private static final String key = "Spring-Boot";
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // request
+        StringBuilder sb = new StringBuilder(); //It is easier to combine string with this data type than String.
+        BufferedReader reader = servletRequest.getReader(); //The reader is for reading HttpRequest
+        String line;
+        while ((line = reader.readLine()) != null) { //Reading line by line until there are nothing remained.
+            sb.append(line);
+        }
+
+        String RequestBody = sb.toString();
+
+        String sha256hex = Hashing.sha256()
+                .hashString(key+RequestBody, StandardCharsets.UTF_8)
+                .toString();
+
+        if (!sha256hex.equals(((HttpServletRequest)servletRequest).getHeader("Authentication"))) {
+            HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+            httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 }
